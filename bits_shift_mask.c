e79c5fb86f006a3426640dbc3272139bebec37bd
Aswini S <ashumeow@live.com>
2014-11-18 15:42:39 +0530

    Update bits_shift_mask.c

--- bits_shift_mask.c
+++ bits_shift_mask.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 #define number_of_bits 8
 #define getline(meow_string, meow_bytes, stdin)
-int main()
+void main()
 {
-char *meow_string;
 int meow_input, meow_args;
 int meow_bytes=100;
 short meow_short, meow_bit;
@@ -14,7 +13,7 @@ meow_input=-1;
 while((meow_args!=1) || (meow_input<0) || (meow_input>128))
 {
 puts("enter an integer from 0 to 128");
-meow_string=(char *) malloc(meow_bytes+1);
+char *meow_string=(char *) malloc(meow_bytes + 1);
 getline(&meow_string, &meow_bytes, stdin);
 meow_args=sscanf(meow_string, "%d", &meow_input);
 if((meow_args!=1) || (meow_input<0) || (meow_input>128))
@@ -29,5 +28,4 @@ printf("%d",meow_bit/meow_mask);
 meow_short<<=1;
 }
 printf("\n");
-return 0;
 }
